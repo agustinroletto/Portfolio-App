c2103e9fbbbff797dba3d0a95baf449ce0044e53
agusroletto <51084228+agustinroletto@users.noreply.github.com>
2021-05-18 21:27:49 -0300

    finish  css

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,52 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { Flex, VStack, Heading } from "@chakra-ui/layout";
+import { IconButton } from "@chakra-ui/button";
+import {
+  FaSun,
+  FaMoon,
+  FaInstagram,
+  FaGithub,
+  FaLinkedin,
+} from "react-icons/fa";
+import { useColorMode } from "@chakra-ui/color-mode";
+import Header from "./components/Header/Header";
+import Social from "./components/Social/Social";
+import Profile from "./components/Profile/Profile";
+import React from "react";
+
+const App = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isDark = colorMode === "dark";
+  return (
+    <VStack p={5}>
+      <Flex w="100%">
+        <Heading ml="8" size="md" fontWeight="semibold" color="cyan.400">
+          Popagusi{" "}
+        </Heading>
+        <a
+          href="https://www.linkedin.com/in/agustinrolettomktdigital/"
+          target="blank"
+        >
+          {" "}
+          <IconButton icon={<FaLinkedin />} isRound="true"></IconButton>
+        </a>
+        <a href="https://github.com/agustinroletto" target="blank">
+          <IconButton ml={2} icon={<FaGithub />} isRound="true"></IconButton>
+        </a>
+        <a href="https://www.instagram.com/agusroletto/" target="blank">
+          <IconButton ml={2} icon={<FaInstagram />} isRound="true"></IconButton>
+        </a>
+        <IconButton
+          ml={8}
+          icon={isDark ? <FaSun /> : <FaMoon />}
+          isRound="true"
+          onClick={toggleColorMode}
+        ></IconButton>
+      </Flex>
+      <Header></Header>
+      <Social></Social>
+      <Profile></Profile>
+    </VStack>
+  );
+};
 
 export default App;
